6a02f5f444f964fca6e063ffe41bec46070139a6
thales <thalesdaniel2013@gmail.com>
2022-02-15 16:50:38 -0300

    config: moved all files

--- src/middlewares/error.js
+++
@@ -1,5 +0,0 @@
-const error = (err, _req, res, _next) => {
-  res.status(err.code || 500).json({ message: err.message });
-};
-
-module.exports = error;
--- src/models/tasks/create.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../connection');
-
-const createTaskt = async (task) => {
-  const connections = await connection();
-  const result = await connections.collection('users').insertOne({ task });
-  return { id: result.insertedId, task };
-};
-
-module.exports = createTaskt;
