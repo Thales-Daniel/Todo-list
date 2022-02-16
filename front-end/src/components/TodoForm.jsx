c5a84db1f1a7a693abcaca03c6161755dc9afcc7
thales <thalesdaniel2013@gmail.com>
2022-02-16 17:20:49 -0300

    feat: create some components

---
+++ front-end/src/components/TodoForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function TodoForm() {
+  const [input, setInput] = useState('');
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setInput('');
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+      />
+      <button type="submit" className="todo-button">
+        Add todo
+      </button>
+
+    </form>
+  );
+}
+
+export default TodoForm;
